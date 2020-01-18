da3173adc2968a8a87056aa96fa8a354525f81e8
Argen <djukeevar@gmail.com>
2020-01-18 12:57:47 +0600

    Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Center',
+              'Hello World!',
             ),
 
           ],
